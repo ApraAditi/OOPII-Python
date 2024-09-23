c38dfea07262f3bb9c027c9bee43b45a994a3002
Apra Rani Das <apra22205101823@diu.edu.bd>
2024-09-23 12:04:45 +0600

    HCF: Highest Common FactHCF: Highest Common Factoror

---
+++ Lab Report/HCF.py
@@ -0,0 +1,15 @@
+# defining a function to calculate HCF  
+def calculate_hcf(x, y):  
+    # selecting the smaller number  
+    if x > y:  
+        smaller = y  
+    else:  
+        smaller = x  
+    for i in range(1,smaller + 1):  
+        if((x % i == 0) and (y % i == 0)):  
+            hcf = i  
+    return hcf  
+  
+num1 = int(input("Enter first number: "))  
+num2 = int(input("Enter second number: "))  
+print("The H.C.F. of", num1,"and", num2,"is", calculate_hcf(num1, num2))  
