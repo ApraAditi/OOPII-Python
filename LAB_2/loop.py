cfdfb4b2f35b6062b46a6c8ca919bb6aa1f4431b
Apra Rani Das <apra22205101823@diu.edu.bd>
2024-12-04 23:55:25 +0600

    Lab task 2.

--- LAB_2/loop.py
+++ LAB_2/loop.py
@@ -11,9 +11,3 @@ for i in range(1, 6):
     print(i)
 
 
-# Using nested for loops to print a multiplication table
-print("\nMultiplication Table:")
-for i in range(1, 6):
-    for j in range(1, 6):
-        print(f"{i} x {j} = {i * j}", end="\t")
-    print()
